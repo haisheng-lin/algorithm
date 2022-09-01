2a249df9a0b4c6064b3a287a097f4f6d6b6eb982
Haisheng Lin <hlin86@asu.edu>
2022-09-01 13:37:24 +0800

    feat: algorithm (#15)
    * fix: reverseArray
    
    * docs: update README.md
    
    * feat: finalPrices
    
    * feat: entry
    
    Co-authored-by: haisheng-lin <linhaisheng@wps.cn>

--- src/two-pointers/reverseArray.ts
+++ src/two-pointers/reverseArray.ts
@@ -1,13 +1,15 @@
 /**
  * 把数组逆序排列
  */
-function reverseArray(items: any[]): void {
+function reverseArray<T>(items: T[]) {
   let start = 0;
   let end = items.length - 1;
   while (start < end) {
     const temp = items[start];
     items[start] = items[end];
     items[end] = temp;
+    start++;
+    end--;
   }
 }
 
