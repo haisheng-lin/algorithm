2466c60552c99681064e915de3e9e6a8a4c9991b
haisheng-lin <linhaisheng@wps.cn>
2022-09-01 13:30:59 +0800

    feat: entry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import { addLargeNumber } from '@/utils';
+import heapSort from './sort/heapSort';
+import quickSort from './sort/quickSort';
+import finalPrices from './stack/finalPrices';
+import containerWithMostWater from './two-pointers/containerWithMostWater';
+import findPair from './two-pointers/findPair';
+import hasCycle from './two-pointers/hasCycle';
+import mergeTwoSortedArrays from './two-pointers/mergeTwoSortedArrays';
+import reverseArray from './two-pointers/reverseArray';
 
-export default addLargeNumber;
+export default {
+  heapSort,
+  quickSort,
+  finalPrices,
+  containerWithMostWater,
+  findPair,
+  hasCycle,
+  mergeTwoSortedArrays,
+  reverseArray
+};
